cf6db9457ded30fd510c21991f1a46ec41176d69
ErwinSturluson <angelmercuries@gmail.com>
2023-09-18 23:02:51 +0600

    [auth][#6] correct the repository interface

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Data/Generic/IRepository.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Data/Generic/IRepository.cs
@@ -1,10 +1,12 @@
 ﻿// Developed and maintained by Erwin Sturluson.
 // Erwin Sturluson licenses this file to you under the MIT license.
 
+using DotNetExtensions.Authorization.OAuth20.Server.Domain.Abstractions;
+
 namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Data.Generic;
 
-public interface IRepository<TEntity>
-    where TEntity : EntityBase
+public interface IRepository<TEntity, TKey>
+    where TEntity : EntityBase<TKey>
 {
     public Task<int> AddAsync(TEntity entity);
 
