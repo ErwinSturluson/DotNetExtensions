aee6fc1b8fa7deb98fc2bbea7eea4de1e8ecccfb
ErwinSturluson <angelmercuries@gmail.com>
2023-09-26 03:02:15 +0600

    [auth][#6] add authorization code & token domain models

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Domain/AuthorizationCode.cs
@@ -0,0 +1,27 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain.Abstractions;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+public class AuthorizationCode : EntityBase<int>
+{
+    public string Value { get; set; } = default!;
+
+    public string? RequestedScope { get; set; }
+
+    public string IssuedScope { get; set; } = default!;
+
+    public string UserId { get; set; } = default!;
+
+    public string ClientId { get; set; } = default!;
+
+    public DateTime IssuanceDateTime { get; set; }
+
+    public int ExpiresIn { get; set; } = 60;
+
+    public DateTime ExpirationDateTime { get; set; }
+
+    public bool Exchanged { get; set; } = false;
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Domain/Token.cs
@@ -0,0 +1,27 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain.Abstractions;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+public class Token : EntityBase<int>
+{
+    public string Value { get; set; } = default!;
+
+    public string Type { get; set; } = default!;
+
+    public string? RequestedScope { get; set; }
+
+    public string IssuedScope { get; set; } = default!;
+
+    public string UserId { get; set; } = default!;
+
+    public string ClientId { get; set; } = default!;
+
+    public DateTime IssuanceDateTime { get; set; }
+
+    public int ExpiresIn { get; set; }
+
+    public DateTime ExpirationDateTime { get; set; }
+}
