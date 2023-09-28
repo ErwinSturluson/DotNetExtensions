dd26783a635afdfef868c9667d9da36953d96a73
ErwinSturluson <angelmercuries@gmail.com>
2023-09-28 23:04:26 +0600

    [auth][#6] add data source abstractions of client, enduser and flow

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataSources/IClientDataSource.cs
@@ -0,0 +1,13 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+
+public interface IClientDataSource
+{
+    public Task<Client?> GetClientAsync(string clientId);
+
+    public Task<IEnumerable<Flow>> GetClientFlowsAsync(string clientId);
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataSources/IFlowDataSource.cs
@@ -0,0 +1,11 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+
+public interface IFlowDataSource
+{
+    public Task<Flow> GetFlowAsync(string name);
+}
