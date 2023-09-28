f46f2eacea4e5b93c32a002bcb71dde278c9b564
ErwinSturluson <angelmercuries@gmail.com>
2023-09-28 23:10:40 +0600

    [auth][#6] add the enduser service

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Services/IEndUserService.cs
@@ -0,0 +1,15 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Services;
+
+public interface IEndUserService
+{
+    public Task<EndUser?> GetCurrentEndUserAsync(string? state = null);
+
+    public Task<EndUser?> GetEndUserAsync(string username);
+
+    public bool IsAuthenticated();
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Default/Services/DefaultEndUserService.cs
@@ -0,0 +1,44 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Errors.Exceptions.Authorize;
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Services;
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Default.Services;
+
+public class DefaultEndUserService : IEndUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IEndUserDataSource _endUserDataSource;
+
+    public DefaultEndUserService(
+        IHttpContextAccessor httpContextAccessor,
+        IEndUserDataSource endUserDataSource)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _endUserDataSource = endUserDataSource;
+    }
+
+    public async Task<EndUser?> GetCurrentEndUserAsync(string? state = null)
+    {
+        string? username = _httpContextAccessor.HttpContext?.User.Identity?.Name;
+
+        if (username is null) throw new AccessDeniedException($"It isn't specified the {username} (Probably the user isn't authenticated).", state);
+
+        return await GetEndUserAsync(username);
+    }
+
+    public async Task<EndUser?> GetEndUserAsync(string username)
+    {
+        return await _endUserDataSource.GetEndUserAsync(username);
+    }
+
+    public bool IsAuthenticated()
+    {
+        var authenticated = _httpContextAccessor.HttpContext?.User.Identity?.IsAuthenticated;
+
+        return !(authenticated != true);
+    }
+}
