6346d677b2f6277db0664068a88e8f3328bdb30f
ErwinSturluson <angelmercuries@gmail.com>
2023-09-28 23:13:48 +0600

    [auth][#6] add storage abstractions of code and token

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataStorages/IAuthorizationCodeStorage.cs
@@ -0,0 +1,13 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Models;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataStorages;
+
+public interface IAuthorizationCodeStorage
+{
+    public Task AddAuthorizationCodeAsync(AuthorizationCode code);
+
+    public Task<AuthorizationCode> GetAuthorizationCodeAsync(string value);
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataStorages/ITokenStorage.cs
@@ -0,0 +1,13 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Models;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataStorages;
+
+public interface ITokenStorage
+{
+    public Task AddTokenAsync(Token token);
+
+    public Task<Token> GetTokenAsync(string value);
+}
