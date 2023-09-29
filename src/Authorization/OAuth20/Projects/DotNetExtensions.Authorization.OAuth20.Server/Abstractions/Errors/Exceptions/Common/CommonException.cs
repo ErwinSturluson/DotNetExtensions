22fe0fd747a254edf886fa761c25c39b29bf3516
ErwinSturluson <angelmercuries@gmail.com>
2023-09-30 03:18:45 +0600

    [auth][#6] add exceptions of common errors

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Errors/Exceptions/Common/CommonException.cs
@@ -0,0 +1,28 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using System.Runtime.Serialization;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Errors.Exceptions.Common;
+
+public class CommonException : OAuth20Exception
+{
+    public CommonException()
+    {
+    }
+
+    public CommonException(string? message, string? state = null)
+        : base(message, state)
+    {
+    }
+
+    public CommonException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+
+    protected CommonException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Errors/Exceptions/Common/InvalidRequestException.cs
@@ -0,0 +1,28 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using System.Runtime.Serialization;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Errors.Exceptions.Common;
+
+public class InvalidRequestException : CommonException
+{
+    public InvalidRequestException()
+    {
+    }
+
+    public InvalidRequestException(string? message, string? state = null)
+        : base(message, state)
+    {
+    }
+
+    public InvalidRequestException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+
+    protected InvalidRequestException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+}
