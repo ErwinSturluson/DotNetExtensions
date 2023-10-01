3d7659f9144d263aa8970f409bd722baa5a80263
ErwinSturluson <angelmercuries@gmail.com>
2023-10-02 02:28:29 +0600

    [auth][#6] add token & auth code providers' abstractions

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Providers/IAuthorizationCodeProvider.cs
@@ -0,0 +1,13 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+using DotNetExtensions.Authorization.OAuth20.Server.Flows.AuthorizationCode.Authorize;
+using DotNetExtensions.Authorization.OAuth20.Server.Models;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Providers;
+
+public interface IAuthorizationCodeProvider
+{
+    public string GetAuthorizationCodeValue(AuthorizeArguments args, EndUser endUser, Client client, string redirectUri, ScopeResult scopeResult);
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Providers/ITokenProvider.cs
@@ -0,0 +1,14 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+using DotNetExtensions.Authorization.OAuth20.Server.Models;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Providers;
+
+public interface ITokenProvider
+{
+    public string GetTokenType(EndUser endUser, Client client, string redirectUri);
+
+    public string GetTokenValue(ScopeResult scopeResult, EndUser endUser, Client client, string redirectUri);
+}
