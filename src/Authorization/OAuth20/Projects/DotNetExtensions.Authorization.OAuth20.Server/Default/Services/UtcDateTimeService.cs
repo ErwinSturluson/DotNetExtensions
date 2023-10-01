a689f82586b1d7a229de4cdd84f8f20cdefb91d4
ErwinSturluson <angelmercuries@gmail.com>
2023-10-01 22:58:46 +0600

    [auth][#6] add the date time service for the UTC time

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Default/Services/UtcDateTimeService.cs
@@ -0,0 +1,11 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Services;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Default.Services;
+
+public class UtcDateTimeService : IDateTimeService
+{
+    public DateTime GetCurrentDateTime() => DateTime.Now;
+}
