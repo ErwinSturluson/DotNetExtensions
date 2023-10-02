3b1fd3f92c5689b6298c22bd843f46d3ff051b51
ErwinSturluson <angelmercuries@gmail.com>
2023-10-02 19:43:54 +0600

    [auth][#6] add the default token type enumeration

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Models/Enums/DefaultTokenType.cs
@@ -0,0 +1,65 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Errors;
+using DotNetExtensions.Authorization.OAuth20.Server.Options;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Models.Enums;
+
+/// <summary>
+/// Enum with the token types are predefined by the server.
+/// </summary>
+public enum DefaultTokenType
+{
+    /// <summary>
+    /// Token type by default. Invalid value for processing.
+    /// </summary>
+    [Description("Token type by default. Invalid value for processing.")]
+    [FieldName("Undefined")]
+    Undefined = 0,
+
+    /// <summary>
+    /// Description RFC7519: <see cref="https://datatracker.ietf.org/doc/html/rfc7617"/>
+    /// </summary>
+    [FieldName("Basic")]
+    Basic = 1,
+
+    /// <summary>
+    /// Description RFC7519: <see cref="https://datatracker.ietf.org/doc/html/rfc7519"/>
+    /// </summary>
+    [FieldName("JWT")]
+    Jwt = 2,
+
+    /// <summary>
+    /// Description RFC7519: <see cref="https://datatracker.ietf.org/doc/html/draft-ietf-oauth-v2-http-mac-05"/>
+    /// </summary>
+    [FieldName("MAC")]
+    Mac = 3,
+}
+
+public static class DefaultTokenTypeExtensions
+{
+    public static string GetFieldNameAttributeValue(this DefaultTokenType defaultTokenType, OAuth20ServerOptions? options)
+    {
+        string tokenType = defaultTokenType switch
+        {
+            DefaultTokenType.Undefined => GetFieldNameAttributeValue(defaultTokenType),
+            DefaultTokenType.Basic => options?.TokenBuilders?.BasicTokenTypeName ?? GetFieldNameAttributeValue(defaultTokenType),
+            DefaultTokenType.Jwt => options?.TokenBuilders?.JwtTokenTypeName ?? GetFieldNameAttributeValue(defaultTokenType),
+            DefaultTokenType.Mac => options?.TokenBuilders?.MacTokenTypeName ?? GetFieldNameAttributeValue(defaultTokenType),
+            _ => throw new NotSupportedException($"{nameof(defaultTokenType)}:{defaultTokenType}"),
+        };
+
+        return tokenType;
+    }
+
+    public static string GetFieldNameAttributeValue(this DefaultTokenType defaultTokenType)
+    {
+        var member = typeof(DefaultTokenType).GetMember(defaultTokenType.ToString()).First();
+        string fieldName = member.GetCustomAttribute<FieldNameAttribute>()!.Name;
+
+        return fieldName;
+    }
+}
