843d49c758ffa375bb64404ebe4eef6cfc1607e6
ErwinSturluson <angelmercuries@gmail.com>
2023-10-05 16:29:20 +0600

    [auth][#6] add the token type data source

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataSources/ITokenTypeDataSource.cs
@@ -0,0 +1,13 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+
+public interface ITokenTypeDataSource
+{
+    public Task<TokenType?> GetTokenTypeAsync(string name);
+
+    public Task<TokenType?> GetTokenTypeAsync(Client client);
+}
