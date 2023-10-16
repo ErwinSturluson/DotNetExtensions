4391deb221abd02efe73f97af6abcf1a07cba669
ErwinSturluson <angelmercuries@gmail.com>
2023-10-16 17:03:34 +0600

    [auth][#6] move FieldNameAttribute to the common directory

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Errors/DefaultTokenErrorType.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Errors/DefaultTokenErrorType.cs
@@ -1,6 +1,7 @@
 ﻿// Developed and maintained by Erwin Sturluson.
 // Erwin Sturluson licenses this file to you under the MIT license.
 
+using DotNetExtensions.Authorization.OAuth20.Server.Attributes;
 using DotNetExtensions.Authorization.OAuth20.Server.Options;
 using System.Reflection;
 
