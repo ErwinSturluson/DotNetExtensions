d9d74e51d28a77634ea35c6e7102c5d40647c6b6
ErwinSturluson <angelmercuries@gmail.com>
2023-10-17 20:49:21 +0600

    [auth][#6] implement the default client secret readers

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/ClientSecretReaders/RequestBodyClientCredentials/DefaultRequestBodyClientCredentialsClientSecretReader.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/ClientSecretReaders/RequestBodyClientCredentials/DefaultRequestBodyClientCredentialsClientSecretReader.cs
@@ -1,14 +1,60 @@
 ﻿// Developed and maintained by Erwin Sturluson.
 // Erwin Sturluson licenses this file to you under the MIT license.
 
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Errors.Exceptions.Token;
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Services;
 using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+using DotNetExtensions.Authorization.OAuth20.Server.Models.Enums;
 
 namespace DotNetExtensions.Authorization.OAuth20.Server.ClientSecretReaders.RequestBodyClientCredentials;
 
 public class DefaultRequestBodyClientCredentialsClientSecretReader : IRequestBodyClientCredentialsClientSecretReader
 {
-    public Task<ClientSecret?> GetClientSecretAsync(HttpContext httpContext)
+    private readonly IClientService _clientService;
+    private readonly IClientSecretDataSource _clientSecretDataSource;
+
+    public DefaultRequestBodyClientCredentialsClientSecretReader(IClientService clientService, IClientSecretDataSource clientSecretDataSource)
     {
-        throw new NotImplementedException();
+        _clientService = clientService;
+        _clientSecretDataSource = clientSecretDataSource;
+    }
+
+    public async Task<ClientSecret?> GetClientSecretAsync(HttpContext httpContext)
+    {
+        ClientSecret? clientSecret = null;
+
+        if (httpContext.Request.Method != HttpMethods.Post)
+        {
+            return clientSecret;
+        }
+
+        var values = httpContext.Request.Form.ToDictionary(x => x.Key, x => x.Value.First()!);
+
+        if (!values.Any())
+        {
+            return clientSecret;
+        }
+
+        if (values.TryGetValue("client_id", out string? requestedClientId))
+        {
+            Client? client = await _clientService.GetClientAsync(requestedClientId);
+
+            if (client is null)
+            {
+                throw new InvalidClientException($"Client with [client_id] = [{requestedClientId}] doesn't exist in the system.");
+            }
+
+            if (values.TryGetValue("client_secret", out string? requestedClientSecret))
+            {
+                clientSecret = await _clientSecretDataSource.GetClientSecretAsync(DefaultClientSecretType.RequestBodyClientCredentials.GetFieldNameAttributeValue(), requestedClientSecret);
+            }
+            else
+            {
+                clientSecret = await _clientSecretDataSource.GetEmptyClientSecretAsync(DefaultClientSecretType.RequestBodyClientCredentials.GetFieldNameAttributeValue(), client);
+            }
+        }
+
+        return clientSecret;
     }
 }
--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/ClientSecretReaders/TlsCertificate/DefaultTlsCertificateClientSecretReader.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/ClientSecretReaders/TlsCertificate/DefaultTlsCertificateClientSecretReader.cs
@@ -1,14 +1,36 @@
 ﻿// Developed and maintained by Erwin Sturluson.
 // Erwin Sturluson licenses this file to you under the MIT license.
 
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
 using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+using DotNetExtensions.Authorization.OAuth20.Server.Models.Enums;
 
 namespace DotNetExtensions.Authorization.OAuth20.Server.ClientSecretReaders.TlsCertificate;
 
 public class DefaultTlsCertificateClientSecretReader : ITlsCertificateClientSecretReader
 {
-    public Task<ClientSecret?> GetClientSecretAsync(HttpContext httpContext)
+    private readonly IClientSecretDataSource _clientSecretDataSource;
+
+    public DefaultTlsCertificateClientSecretReader(IClientSecretDataSource clientSecretDataSource)
     {
-        throw new NotImplementedException();
+        _clientSecretDataSource = clientSecretDataSource;
+    }
+
+    public async Task<ClientSecret?> GetClientSecretAsync(HttpContext httpContext)
+    {
+        ClientSecret? clientSecret = null;
+
+        var clientCertificate = await httpContext.Connection.GetClientCertificateAsync();
+
+        if (clientCertificate is null)
+        {
+            return clientSecret;
+        }
+
+        string clientSecretContent = clientCertificate.GetRawCertDataString();
+
+        await _clientSecretDataSource.GetClientSecretAsync(DefaultClientSecretType.TlsCertificate.GetFieldNameAttributeValue(), clientSecretContent);
+
+        return clientSecret;
     }
 }
