259970e32e07aa1f9f70c28a7491ca2aa2004948
ErwinSturluson <angelmercuries@gmail.com>
2023-10-18 13:01:54 +0600

    [auth][#6] correct the token flow interface

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Flows/ITokenFlow.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Flows/ITokenFlow.cs
@@ -1,6 +1,7 @@
 ﻿// Developed and maintained by Erwin Sturluson.
 // Erwin Sturluson licenses this file to you under the MIT license.
 
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
 using DotNetExtensions.Authorization.OAuth20.Server.Flows;
 
 namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Flows;
@@ -10,5 +11,5 @@ namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Flows;
 /// </summary>
 public interface ITokenFlow : IFlow
 {
-    public Task<IResult> GetTokenAsync(FlowArguments args);
+    public Task<IResult> GetTokenAsync(FlowArguments args, Client client);
 }
