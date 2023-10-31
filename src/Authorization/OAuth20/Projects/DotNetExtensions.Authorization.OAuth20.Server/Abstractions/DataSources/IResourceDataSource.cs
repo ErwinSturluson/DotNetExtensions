3d7ac0c429caf95b0c6126152054117a46e63162
ErwinSturluson <angelmercuries@gmail.com>
2023-10-31 18:06:13 +0600

    [auth][#6] add the resource data source

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataSources/IResourceDataSource.cs
@@ -0,0 +1,13 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+
+public interface IResourceDataSource
+{
+    public Task<Resource> GetResourceByScopeAsync(Scope scope);
+
+    public Task<IEnumerable<SigningCredentialsAlgorithm>> GetResourceSigningCredentialsAlgorithmsAsync(Resource resource);
+}
