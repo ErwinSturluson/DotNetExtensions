b65d1144c7a544392e50f09848275d7c34f730f4
ErwinSturluson <angelmercuries@gmail.com>
2023-10-31 18:10:44 +0600

    [auth][#6] add the token builder context

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/TokenBuilders/TokenBuilderContext.cs
@@ -0,0 +1,29 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.TokenBuilders;
+
+public class TokenBuilderContext
+{
+    public IEnumerable<Scope> Scopes { get; set; } = default!;
+
+    public Client Client { get; set; } = default!;
+
+    public DateTimeOffset? CreationDateTime { get; set; }
+
+    public DateTimeOffset? ActivationDateTime { get; set; }
+
+    public DateTimeOffset? ExpirationDateTime { get; set; }
+
+    public long? LifetimeSeconds { get; set; }
+
+    public string? Issuer { get; set; }
+
+    public IEnumerable<string>? Audiences { get; set; }
+
+    public IDictionary<string, string>? AdditionalParameters { get; set; }
+
+    public EndUser? EndUser { get; set; }
+}
