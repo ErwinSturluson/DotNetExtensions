9305008a003871b4572e62e9ac3ff3a63d21018e
ErwinSturluson <angelmercuries@gmail.com>
2023-10-31 19:59:28 +0600

    [auth][#6] add client secret service

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Default/Services/DefaultClientSecretService.cs
@@ -0,0 +1,28 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Services;
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Default.Services;
+
+public class DefaultClientSecretService : IClientSecretService
+{
+    private readonly IClientSecretDataSource _clientSecretDataSource;
+
+    public DefaultClientSecretService(IClientSecretDataSource clientSecretDataSource)
+    {
+        _clientSecretDataSource = clientSecretDataSource;
+    }
+
+    public async Task<ClientSecret?> GetClientSecretAsync(string type, string clientSecretContent)
+    {
+        return await _clientSecretDataSource.GetClientSecretAsync(type, clientSecretContent);
+    }
+
+    public async Task<ClientSecret?> GetEmptyClientSecretAsync(string type, Client client)
+    {
+        return await _clientSecretDataSource.GetEmptyClientSecretAsync(type, client);
+    }
+}
