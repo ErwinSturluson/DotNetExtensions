f37d58ded4159047e9f58a3c5be7d221ee98bce3
ErwinSturluson <angelmercuries@gmail.com>
2023-11-05 12:09:15 +0600

    [auth][#6] remove unused code

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/Builders/Generic/IResultBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿// Developed and maintained by Erwin Sturluson.
-// Erwin Sturluson licenses this file to you under the MIT license.
-
-namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Builders.Generic;
-
-public interface IResultBuilder<TResult> : IResultBuilder
-{
-    public ValueTask<IResult> BuildResultAsync(TResult result);
-}
