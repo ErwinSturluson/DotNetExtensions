072aa04c7177b0fcda9b2e4c565017142a49da56
ErwinSturluson <angelmercuries@gmail.com>
2023-11-07 15:57:03 +0600

    [auth][#6] add flow response & grant type domain models

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Domain/FlowResponseType.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Domain/FlowResponseType.cs
@@ -5,19 +5,11 @@ using DotNetExtensions.Authorization.OAuth20.Server.Domain.Abstractions;
 
 namespace DotNetExtensions.Authorization.OAuth20.Server.Domain;
 
-public class Flow : EntityBase<int>
+public class FlowResponseType : EntityBase<int>
 {
     public string Name { get; set; } = default!;
 
     public string Description { get; set; } = default!;
 
-    public int? FlowGrantTypeId { get; set; }
-
-    public FlowGrantType? FlowGrantType { get; set; }
-
-    public int? FlowResponseTypeId { get; set; }
-
-    public FlowResponseType? FlowResponseType { get; set; }
-
-    public IEnumerable<ClientFlow>? ClientFlows { get; set; }
+    public IEnumerable<Flow>? Flows { get; set; }
 }
