97aeef76e7cf933b3d6d3f8d251275614afcebf9
ErwinSturluson <angelmercuries@gmail.com>
2023-11-13 00:23:48 +0600

    [auth][#6] add in-memory data source context

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server.InMemory/InMemoryDataSourceContext.cs
@@ -0,0 +1,24 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataSources;
+using DotNetExtensions.Authorization.OAuth20.Server.InMemory.DataSources;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.InMemory;
+
+public class InMemoryDataSourceContext : IDataSourceContext
+{
+    public Type ClientDataSourceType { get; set; } = typeof(InMemoryClientDataSource);
+
+    public Type ClientSecretDataSourceType { get; set; } = typeof(InMemoryClientSecretDataSource);
+
+    public Type EndUserDataSourceType { get; set; } = typeof(InMemoryEndUserDataSource);
+
+    public Type FlowDataSourceType { get; set; } = typeof(InMemoryFlowDataSource);
+
+    public Type ResourceDataSourceType { get; set; } = typeof(InMemoryResourceDataSource);
+
+    public Type ScopeDataSourceType { get; set; } = typeof(InMemoryScopeDataSource);
+
+    public Type TokenTypeDataSourceType { get; set; } = typeof(InMemoryTokenTypeDataSource);
+}
