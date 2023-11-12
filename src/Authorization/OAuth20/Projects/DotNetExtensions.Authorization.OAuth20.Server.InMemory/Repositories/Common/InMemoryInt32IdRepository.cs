525be64e6b7abb92821f69efefabfa7cb5731d36
ErwinSturluson <angelmercuries@gmail.com>
2023-11-13 00:22:46 +0600

    [auth][#6] add in-memory repositories

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server.InMemory/Repositories/Common/InMemoryInt32IdRepository.cs
@@ -0,0 +1,16 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Reporitories;
+using DotNetExtensions.Authorization.OAuth20.Server.Domain.Abstractions;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.InMemory.Repositories.Common;
+
+public class InMemoryInt32IdRepository<TEntity> : InMemoryRepository<TEntity, int>, IInt32IdRepository<TEntity>
+    where TEntity : Int32IdEntityBase, new()
+{
+    public InMemoryInt32IdRepository(OAuth20ServerDbContext oAuth20ServerDbContext)
+        : base(oAuth20ServerDbContext)
+    {
+    }
+}
