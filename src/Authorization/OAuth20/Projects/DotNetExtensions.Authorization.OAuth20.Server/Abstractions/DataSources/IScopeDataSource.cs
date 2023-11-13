3917c157957187f053c51fba48dc9476120444a2
ErwinSturluson <angelmercuries@gmail.com>
2023-11-13 23:05:01 +0600

    [auth][#6] add an impersonal method to scope data source

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataSources/IScopeDataSource.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataSources/IScopeDataSource.cs
@@ -12,4 +12,6 @@ public interface IScopeDataSource
     public Task<IEnumerable<Scope>> GetScopeListAsync(IEnumerable<string> names);
 
     public Task<IEnumerable<Scope>> GetScopesAsync(EndUser endUser, Client client);
+
+    public Task<IEnumerable<Scope>> GetScopesAsync(Client client);
 }
