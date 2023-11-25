1943ac9d3328246c7c02e288d73b1edb0971f626
ErwinSturluson <angelmercuries@gmail.com>
2023-11-25 21:48:58 +0300

    [auth][#7] add an HTML page result's implementation

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/WebPageBuilders/HtmlPageResult.cs
@@ -0,0 +1,22 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.WebPageBuilders;
+
+public class HtmlResult : IResult
+{
+    private readonly string _htmlContent;
+
+    public HtmlResult(string htmlContent)
+    {
+        _htmlContent = htmlContent;
+    }
+
+    public async Task ExecuteAsync(HttpContext httpContext)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status200OK;
+        httpContext.Response.ContentType = "text/html";
+
+        await httpContext.Response.WriteAsync(_htmlContent);
+    }
+}
