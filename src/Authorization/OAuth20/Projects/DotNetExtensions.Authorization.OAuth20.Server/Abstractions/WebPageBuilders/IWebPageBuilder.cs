c59f8a3131e5a61c3e1a5167ea4baf1c1bfcf97e
ErwinSturluson <angelmercuries@gmail.com>
2023-11-25 21:13:38 +0300

    [auth][#7] add web page builder's abstractions

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/WebPageBuilders/IWebPageBuilder.cs
@@ -0,0 +1,9 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Endpoints;
+
+public interface IWebPageBuilder
+{
+    public Task<IResult> InvokeAsync(HttpContext httpContext);
+}
---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/WebPageBuilders/IWebPageBuilderProvider.cs
@@ -0,0 +1,9 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Endpoints;
+
+public interface IWebPageBuilderProvider
+{
+    public bool TryGetWebPageBuilderInstanceByRoute(string route, out IWebPageBuilder? webPageBuilder);
+}
