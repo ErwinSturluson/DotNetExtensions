23c82fb651af249427214e6e27f62eaebeeb1fa6
ErwinSturluson <angelmercuries@gmail.com>
2023-12-08 17:10:13 +0300

    [auth][#12] add end-user client scope's request & result

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Models/EndUserClientScopeResult.cs
@@ -0,0 +1,17 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Models;
+
+public class EndUserClientScopeResult
+{
+    public string Username { get; set; } = default!;
+
+    public string ClientId { get; set; } = default!;
+
+    public string ServerAllowedScope { get; set; } = default!;
+
+    public string EndUserIssuedScope { get; set; } = default!;
+
+    public bool EndUserIssuedScopeDifferent { get; set; } = false;
+}
