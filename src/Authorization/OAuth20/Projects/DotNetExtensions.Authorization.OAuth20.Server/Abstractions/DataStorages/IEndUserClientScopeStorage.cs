0710abdeb85f938a2412d4877b8597b5e967a53d
ErwinSturluson <angelmercuries@gmail.com>
2023-12-08 17:49:12 +0300

    [auth][#12] add abstraction of end-user client scope storage

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Abstractions/DataStorages/IEndUserClientScopeStorage.cs
@@ -0,0 +1,17 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Models;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Abstractions.DataStorages;
+
+public interface IEndUserClientScopeStorage
+{
+    public Task AddEndUserClientScopeRequestAsync(EndUserClientScopeRequest endUserClientScopeRequest);
+
+    public Task<EndUserClientScopeRequest?> GetEndUserClientScopeRequestAsync(string username, string clientId);
+
+    public Task AddEndUserClientScopeResultAsync(EndUserClientScopeResult endUserClientScopeResult);
+
+    public Task<EndUserClientScopeResult?> GetEndUserClientScopeResultAsync(string username, string clientId);
+}
