42590b27779db2cc7f4b6b1c55e7c627ac78f191
ErwinSturluson <angelmercuries@gmail.com>
2023-12-13 01:37:28 +0300

    [auth][#12] add default redirect result

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Default/Endpoints/DefaultRedirectResult.cs
@@ -0,0 +1,37 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Endpoints;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Default.Endpoints;
+
+public class DefaultRedirectResult : RedirectResultBase
+{
+    public DefaultRedirectResult()
+    {
+    }
+
+    public DefaultRedirectResult(string redirectUri, IDictionary<string, string>? queryParameters = null)
+        : base(redirectUri, queryParameters)
+    {
+    }
+
+    public override Task ExecuteAsync(HttpContext httpContext)
+    {
+        string redirectLocation;
+
+        if (QueryParameters is not null && QueryParameters.Any())
+        {
+            string queryString = string.Join("&", QueryParameters.Select(x => $"{Uri.EscapeDataString(x.Key)}={Uri.EscapeDataString(x.Value)}"));
+            redirectLocation = RedirectUri + "?" + queryString;
+        }
+        else
+        {
+            redirectLocation = RedirectUri;
+        }
+
+        httpContext.Response.Redirect(redirectLocation, false, false);
+
+        return Task.CompletedTask;
+    }
+}
