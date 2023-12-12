5a46fddef8d3e9fa569a1730d00893182212dd71
ErwinSturluson <angelmercuries@gmail.com>
2023-12-13 01:46:41 +0300

    [auth][#12] update login redirect result

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Flows/LoginRedirectResult.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Flows/LoginRedirectResult.cs
@@ -1,12 +1,11 @@
 ﻿// Developed and maintained by Erwin Sturluson.
 // Erwin Sturluson licenses this file to you under the MIT license.
 
-using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Flows;
-using System.Text;
+using DotNetExtensions.Authorization.OAuth20.Server.Default.Endpoints;
 
 namespace DotNetExtensions.Authorization.OAuth20.Server.Flows;
 
-public class LoginRedirectResult : RedirectResultBase
+public class LoginRedirectResult : DefaultRedirectResult
 {
     public LoginRedirectResult(string loginEndpoint)
         : base(loginEndpoint)
@@ -16,49 +15,13 @@ public class LoginRedirectResult : RedirectResultBase
     public LoginRedirectResult(string loginEndpoint, FlowArguments flowArguments, IDictionary<string, string>? additionalParameters = null)
         : base(loginEndpoint)
     {
-        FlowArguments = flowArguments;
-        AdditionalParameters = additionalParameters;
-    }
+        QueryParameters = flowArguments.Values;
 
-    public FlowArguments FlowArguments { get; set; } = default!;
-
-    public IDictionary<string, string>? AdditionalParameters { get; }
-
-    public override Task ExecuteAsync(HttpContext httpContext)
-    {
-        StringBuilder stringBuilder = new(RedirectUri);
-
-        IEnumerable<KeyValuePair<string, string>> localDictionary = FlowArguments.Values;
-
-        if (AdditionalParameters is not null)
+        if (additionalParameters is not null && additionalParameters.Any())
         {
-            localDictionary = localDictionary.Concat(AdditionalParameters);
+            QueryParameters = QueryParameters.Concat(additionalParameters).ToDictionary();
         }
 
-        if (localDictionary.Any())
-        {
-            stringBuilder.Append('?');
-
-            bool firstElement = true;
-
-            foreach (var item in localDictionary)
-            {
-                if (firstElement)
-                {
-                    stringBuilder.AppendFormat("{0}={1}", item.Key, item.Value);
-                    firstElement = false;
-                }
-                else
-                {
-                    stringBuilder.AppendFormat("&{0}={1}", item.Key, item.Value);
-                }
-            }
-        }
-
-        string redirectLocation = stringBuilder.ToString();
-
-        httpContext.Response.Redirect(redirectLocation, false, false);
-
-        return Task.CompletedTask;
+        QueryParameters.Add("oauth20_server_redirect", "/oauth/authorize");
     }
 }
