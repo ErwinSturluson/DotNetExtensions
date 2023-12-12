6e3527f2bbcb7cd2b25d15df48f9bb52d4987a39
ErwinSturluson <angelmercuries@gmail.com>
2023-12-13 02:12:28 +0300

    [auth][#12] update authorization endpoint

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Endpoints/Authorization/DefaultAuthorizationEndpoint.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Endpoints/Authorization/DefaultAuthorizationEndpoint.cs
@@ -5,7 +5,10 @@ using DotNetExtensions.Authorization.OAuth20.Server.Abstractions;
 using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Builders.Generic;
 using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Errors;
 using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Flows;
+using DotNetExtensions.Authorization.OAuth20.Server.Abstractions.Services;
+using DotNetExtensions.Authorization.OAuth20.Server.Domain;
 using DotNetExtensions.Authorization.OAuth20.Server.Flows;
+using DotNetExtensions.Authorization.OAuth20.Server.Models;
 using DotNetExtensions.Authorization.OAuth20.Server.Options;
 using Microsoft.Extensions.Options;
 
@@ -20,6 +23,11 @@ public class DefaultAuthorizationEndpoint : IAuthorizationEndpoint
     private readonly IArgumentsBuilder<FlowArguments> _flowArgsBuilder;
     private readonly IRequestValidator<IAuthorizationEndpoint> _requestValidator;
     private readonly IErrorResultProvider _errorResultProvider;
+    private readonly IClientService _clientService;
+    private readonly ILoginService _loginService;
+    private readonly IEndUserService _endUserService;
+    private readonly IScopeService _scopeService;
+    private readonly IPermissionsService _permissionsService;
     private readonly IOptions<OAuth20ServerOptions> _options;
 
     public DefaultAuthorizationEndpoint(
@@ -27,12 +35,22 @@ public class DefaultAuthorizationEndpoint : IAuthorizationEndpoint
         IArgumentsBuilder<FlowArguments> flowArgsBuilder,
         IRequestValidator<IAuthorizationEndpoint> requestValidator,
         IErrorResultProvider errorResultProvider,
+        IClientService clientService,
+        ILoginService loginService,
+        IEndUserService endUserService,
+        IScopeService scopeService,
+        IPermissionsService permissionsService,
         IOptions<OAuth20ServerOptions> options)
     {
         _flowRouter = flowRouter;
         _flowArgsBuilder = flowArgsBuilder;
         _requestValidator = requestValidator;
         _errorResultProvider = errorResultProvider;
+        _clientService = clientService;
+        _loginService = loginService;
+        _endUserService = endUserService;
+        _scopeService = scopeService;
+        _permissionsService = permissionsService;
         _options = options;
     }
 
@@ -41,25 +59,85 @@ public class DefaultAuthorizationEndpoint : IAuthorizationEndpoint
         FlowArguments flowArgs = await _flowArgsBuilder.BuildArgumentsAsync(httpContext);
         var validationResult = _requestValidator.TryValidate(httpContext);
 
+        if (!flowArgs.Values.TryGetValue("state", out string? state) && _options.Value.AuthorizationRequestStateRequired)
+        {
+            return _errorResultProvider.GetAuthorizeErrorResult(
+                DefaultAuthorizeErrorType.InvalidRequest,
+                state: null,
+                "Missing request parameter: [state]");
+        }
+
         if (!validationResult.Success)
         {
-            flowArgs.Values.TryGetValue("state", out string? state);
             return _errorResultProvider.GetAuthorizeErrorResult(DefaultAuthorizeErrorType.InvalidRequest, state, null);
         }
 
         if (!flowArgs.Values.TryGetValue("response_type", out string? responseType))
         {
-            flowArgs.Values.TryGetValue("state", out string? state);
             return _errorResultProvider.GetAuthorizeErrorResult(DefaultAuthorizeErrorType.InvalidRequest, state, null);
+        }
+
+        if (!flowArgs.Values.TryGetValue("client_id", out string? clientId))
+        {
+            return _errorResultProvider.GetAuthorizeErrorResult(DefaultAuthorizeErrorType.InvalidRequest, state, null);
+        }
+
+        Client? client = await _clientService.GetClientAsync(clientId);
+        if (client is null)
+        {
+            return _errorResultProvider.GetAuthorizeErrorResult(
+                DefaultAuthorizeErrorType.UnauthorizedClient,
+                state,
+                $"Client with [client_id] = [{clientId}] doesn't exist in the system.");
+        }
+
+        if (!_endUserService.IsAuthenticated())
+        {
+            return await _loginService.RedirectToLoginAsync(flowArgs);
+        }
+
+        var endUser = await _endUserService.GetCurrentEndUserAsync();
+        if (endUser is null)
+        {
+            return _errorResultProvider.GetAuthorizeErrorResult(
+                DefaultAuthorizeErrorType.UnauthorizedClient,
+                state,
+                "Current EndUser doesn't exist in the system.");
+        }
+
+        flowArgs.Values.TryGetValue("scope", out string? clientRequestedScope);
+
+        bool redirectToPermissionsRequired = await _permissionsService.RedirectToPermissionsRequiredAsync(endUser, client);
+        if (redirectToPermissionsRequired)
+        {
+            ScopeResult serverAllowedScopeResult = await _scopeService.GetServerAllowedScopeAsync(clientRequestedScope, client, state);
+            await _permissionsService.AddPermissionsRequestAsync(serverAllowedScopeResult, endUser, client);
+
+            return await _permissionsService.RedirectToPermissionsAsync(flowArgs, client, state);
+        }
+
+        ScopeResult scopeResult;
+        bool endUserPermissionsReuired = await _permissionsService.EndUserPermissionsRequiredAsync(client);
+        if (endUserPermissionsReuired)
+        {
+            scopeResult = await _scopeService.GetEndUserClientScopeAsync(clientRequestedScope, endUser, client, state);
+        }
+        else
+        {
+            scopeResult = await _scopeService.GetServerAllowedScopeAsync(clientRequestedScope, client, state);
         }
 
         if (_flowRouter.TryGetAuthorizeFlow(responseType, out IAuthorizeFlow? flow))
         {
-            return await flow!.AuthorizeAsync(flowArgs);
+            if (flow is null)
+            {
+                return _errorResultProvider.GetAuthorizeErrorResult(DefaultAuthorizeErrorType.ServerError, state, "Cannot determine the flow.");
+            }
+
+            return await flow.AuthorizeAsync(flowArgs, client, endUser, scopeResult);
         }
         else
         {
-            flowArgs.Values.TryGetValue("state", out string? state);
             // TODO: handle only this node unsupported response type
             return _errorResultProvider.GetAuthorizeErrorResult(DefaultAuthorizeErrorType.UnsupportedResponseType, state, null);
         }
