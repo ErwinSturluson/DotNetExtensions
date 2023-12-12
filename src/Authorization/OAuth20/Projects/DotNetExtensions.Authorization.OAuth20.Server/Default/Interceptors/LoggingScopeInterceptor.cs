e7e23d324abd4ade1c4bca3e14f254a3b2beb545
ErwinSturluson <angelmercuries@gmail.com>
2023-12-13 00:41:31 +0300

    [auth][#12] update the scope interceptor

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Default/Interceptors/LoggingScopeInterceptor.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Default/Interceptors/LoggingScopeInterceptor.cs
@@ -16,30 +16,30 @@ public class LoggingScopeInterceptor : IScopeInterceptor
         _logger = logger;
     }
 
-    public Task<IEnumerable<Scope>> OnExecutedAsync(IEnumerable<Scope> issuedScope, Client client, EndUser? endUser = null, string? state = null)
+    public Task<IEnumerable<Scope>> OnExecutedAsync(IEnumerable<Scope> issuedScope, Client client, string? state = null)
     {
-        _logger.LogInformation("Issued Scope Models for State [{state}], Client [{client}] and EndUser [{endUser}]: [{issuedScope}]", state, client, endUser, JsonSerializer.Serialize(issuedScope));
+        _logger.LogInformation("Issued Scope Models for State [{state}] and Client [{client}]: [{issuedScope}]", state, client, JsonSerializer.Serialize(issuedScope));
 
         return Task.FromResult(issuedScope);
     }
 
-    public Task<string> OnExecutedAsync(string issuedScope, Client client, EndUser? endUser = null, string? state = null)
+    public Task<string> OnExecutedAsync(string issuedScope, Client client, string? state = null)
     {
-        _logger.LogInformation("Issued Scope Models for State [{state}], Client [{client}] and EndUser [{endUser}]: [{issuedScope}]", state, client, endUser, issuedScope);
+        _logger.LogInformation("Issued Scope Models for State [{state}]and Client [{client}]: [{issuedScope}]", state, client, issuedScope);
 
         return Task.FromResult(issuedScope);
     }
 
-    public Task<string?> OnExecutingAsync(string? requestedScope, Client client, EndUser? endUser = null, string? state = null)
+    public Task<string?> OnExecutingAsync(string? requestedScope, Client client, string? state = null)
     {
-        _logger.LogInformation("Issued Scope Models for State [{state}], Client [{client}] and EndUser [{endUser}]: [{requestedScope}]", state, client, endUser, requestedScope);
+        _logger.LogInformation("Issued Scope Models for State [{state}]and Client [{client}]: [{requestedScope}]", state, client, requestedScope);
 
         return Task.FromResult(requestedScope);
     }
 
-    public Task<IEnumerable<Scope>> OnExecutingAsync(IEnumerable<Scope> requestedScope, Client client, EndUser? endUser = null, string? state = null)
+    public Task<IEnumerable<Scope>> OnExecutingAsync(IEnumerable<Scope> requestedScope, Client client, string? state = null)
     {
-        _logger.LogInformation("Issued Scope Models for State [{state}], Client [{client}] and EndUser [{endUser}]: [{requestedScope}]", state, client, endUser, JsonSerializer.Serialize(requestedScope));
+        _logger.LogInformation("Issued Scope Models for State [{state}]and Client [{client}]: [{requestedScope}]", state, client, JsonSerializer.Serialize(requestedScope));
 
         return Task.FromResult(requestedScope);
     }
