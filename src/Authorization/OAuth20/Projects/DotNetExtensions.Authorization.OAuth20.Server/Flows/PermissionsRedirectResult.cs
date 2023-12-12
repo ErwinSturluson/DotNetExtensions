6107a9c0a8cd2d10e952f5e4fa344a0d63994528
ErwinSturluson <angelmercuries@gmail.com>
2023-12-13 01:37:53 +0300

    [auth][#12] add permissions redirect result

---
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server/Flows/PermissionsRedirectResult.cs
@@ -0,0 +1,27 @@
+﻿// Developed and maintained by Erwin Sturluson.
+// Erwin Sturluson licenses this file to you under the MIT license.
+
+using DotNetExtensions.Authorization.OAuth20.Server.Default.Endpoints;
+
+namespace DotNetExtensions.Authorization.OAuth20.Server.Flows;
+
+public class PermissionsRedirectResult : DefaultRedirectResult
+{
+    public PermissionsRedirectResult(string permissionsEndpoint)
+        : base(permissionsEndpoint)
+    {
+    }
+
+    public PermissionsRedirectResult(string permissionsEndpoint, FlowArguments flowArguments, IDictionary<string, string>? additionalParameters = null)
+        : base(permissionsEndpoint)
+    {
+        QueryParameters = flowArguments.Values;
+
+        if (additionalParameters is not null && additionalParameters.Any())
+        {
+            QueryParameters = QueryParameters.Concat(additionalParameters).ToDictionary();
+        }
+
+        QueryParameters.Add("oauth20_server_redirect", "/oauth/authorize");
+    }
+}
