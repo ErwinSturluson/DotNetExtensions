1a4d21633742e23c2ef4eed754de3f3fb7cf48d4
ErwinSturluson <angelmercuries@gmail.com>
2024-01-05 00:38:01 +0300

    [auth][#14] correct the StaticFilesLinkResolver class

--- src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server.Account/StaticFilesLinkResolver.cs
+++ src/Authorization/OAuth20/Projects/DotNetExtensions.Authorization.OAuth20.Server.Account/StaticFilesLinkResolver.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Microsoft.Extensions.FileProviders;
+using System.Reflection;
 
 namespace DotNetExtensions.Authorization.OAuth20.Server.Account;
 
@@ -7,7 +8,7 @@ public sealed class StaticFilesLinkResolver
     private string _leadingUriSegments;
     private string _appCssFileName;
 
-    public StaticFilesLinkResolver()
+    public StaticFilesLinkResolver(IWebHostEnvironment env)
     {
         string blazorAppName = Assembly.GetExecutingAssembly().GetName().Name ??
             "DotNetExtensions.Authorization.OAuth20.Server.Account";
@@ -15,10 +16,17 @@ public sealed class StaticFilesLinkResolver
         string? appName = Assembly.GetEntryAssembly()?.GetName().Name;
 
         _leadingUriSegments = appName != blazorAppName && appName is not null ?
-            "_content/" + appName + "/" :
+            "_content/" + blazorAppName + "/" :
             string.Empty;
 
         _appCssFileName = appName ?? blazorAppName;
+
+#if DEBUG
+        var rootProvider = env.WebRootFileProvider as CompositeFileProvider;
+        var provider = rootProvider!.FileProviders.FirstOrDefault(x => x.GetType().Name == "ManifestStaticWebAssetFileProvider")!;
+        var rootNodes = provider.GetType().GetRuntimeFields().FirstOrDefault(x => x.Name == "_root")!.GetValue(provider)!;
+        var children = rootNodes.GetType().GetProperty("Children")!.GetValue(rootNodes);
+#endif
     }
 
     public string ResolveStaticFileLink(string sourceLink) => _leadingUriSegments + sourceLink;
